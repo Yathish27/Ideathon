f98f40b1eebc0a8f7cca783080cc8b91e3bf35e4
Yathish N V <48596315+Yathish27@users.noreply.github.com>
2020-10-23 14:59:49 +0530

    project added

---
+++ confnew.py
@@ -0,0 +1,3 @@
+device_id = "BOLT4628336"                    # This is the device ID and will b>
+telegram_chat_id = "@ideathon"            # This is the channel ID of the crea>
+telegram_bot_id = "bot1371882065:AAHejdhLII5ib4TVf7JHC7lICYqoH4ZcrgY"
